e1a0b496f3e31c6c302b33c1bfa5726648d9bfea
malar <malarsr2001@gmail.com>
2014-04-27 20:10:30 -0700

    second update of CachyMatrix code

--- cachematrix.R
+++ cachematrix.R
@@ -6,24 +6,24 @@
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(inv) m <<- inv
-  getInverse <- function() m
+    x <<- y    #sets a matrix to a function set
+    m <<- NULL #<<- refers to m in parent environment; 
+  }            #m is again set null here and y is assigned to x created/called in parent env if the matrix is created using set function
+  get <- function() x   #gets the matrix to a function get
+  setInverse <- function(inv) m <<- inv  #sets the inverse of a matrix to m in parent env
+  getInverse <- function() m ##gets the inverse matrix m
   list(set = set, get = get,
        setInverse = setInverse,
-       getInverse = getInverse)
+       getInverse = getInverse) #all the functions are called using list
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  m <- x$getInverse() #query the x matrix cache 
-  if(!is.null(m)) {   #if there is a cache
-    message("getting cached data")  #just return the cache, no computation needed
+  m <- x$getInverse() #query the x matrix already cached using makeCacheMatrix function
+  if(!is.null(m)) {   #if the inverse is already created
+    message("getting cached data")  #just return the message, no computation needed
     return(m)
   }
   data <- x$get()    #if there's no cache
